fae0da1155781106cc695ab20ab0c7108b242381
brijiith <brijith@neoito.com>
2022-02-21 09:16:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import picture from './picture.png'
 import './App.css';
 
 function App() {
+const [imgPreview, setImgPreview] = useState()
+const [error,setError]= useState(false)
+  const imageChange = (e) =>{
+const selectedImg= e.target.files[0]
+const valid_types = ["image/png", 'image/jpeg', "image/jpg"]
+if (selectedImg && valid_types.includes(selectedImg.type)){
+ let reader = new FileReader()
+ reader.onlodend=()=>{
+setImgPreview(reader.result)
+
+ }
+ reader.readAsDataURL(selectedImg)
+} else
+setError(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className='wrap'>
+        {error && <p className='errorMessage'>File not supported</p>}
+        <div className='imgPreview'
+        style={{
+          background: imgPreview
+          ? 'url("${imgPreview}") no-repeat center/cover'
+          : "ivory"
+      }}
         >
-          Learn React
-        </a>
-      </header>
+
+        {!imgPreview && (
+          <><input
+              type="file"
+              className="hidden"
+              id="commentImage"
+              onChange={imageChange}
+              accept="image/*" />
+              <label className='shown' for="commentImage">
+                <div className='topper'>
+                  <img src={picture} />
+                </div>
+                <h1 className='text-2xl text-amber-400'>UPLOAD IMAGE</h1>
+              </label>
+              <input type="text" placeholder="Caption" classname="showInput" /></>
+        )}
+                
+                </div>
+                {imgPreview && (
+                  <button onClick={()=> setImgPreview(null)}> X </button>
+                )}
+                </div>
     </div>
   );
 }
